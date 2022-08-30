a9eebba854ef70e6f922419f409828a31c6453d8
rickfbq <63530968+rickfbq@users.noreply.github.com>
2022-08-30 10:33:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,15 +51,15 @@ def get_weather(region):
         # 获取地区的location--id
         location_id = response["location"][0]["id"]
     weather_url1 = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
-    # weather_url2 = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
+    weather_url2 = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
     response1 = get(weather_url1, headers=headers).json()
-    # response2 = get(weather_url2, headers=headers).json()
+    response2 = get(weather_url2, headers=headers).json()
     # 天气
     weather = response1["now"]["text"]
     # 当前温度
     temp = response1["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
-    # tempMax = response1["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
-    # tempMin = response1["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    tempMax = response1["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    tempMin = response1["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response1["now"]["windDir"]
     return weather, temp, wind_dir
